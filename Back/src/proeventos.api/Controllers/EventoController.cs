35dd097c641369c15ef5e41a1de90c9b582066c9
David Pinheiro Antunes <davidpantunes115@gmail.com>
2021-12-23 17:10:52 -0300

    First Comitada
    O primeiro Commit

---
+++ Back/src/proeventos.api/Controllers/EventoController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using proeventos.api.Models;
+
+namespace proeventos.api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+public IEnumerable<Evento> _evento = new Evento[]{
+                new Evento(){
+                    EventoId = 1,
+                    Tema = "Angular e .NET 5",
+                    Local = "SãoPaulo",
+                    Lote = "Primeiro Lote",
+                    QntPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString()
+                },
+                new Evento(){
+                    EventoId = 2,
+                    Tema = "Angular12 e .NET 59",
+                    Local = "Rio de janeiro",
+                    Lote = "Quinto Lote",
+                    QntPessoas = 9082,
+                    DataEvento = DateTime.Now.AddDays(5).ToString()
+                } 
+            };
+
+
+
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+         return _evento;   
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "post works"; 
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"put works, id = {id}"; 
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Delete works, id = {id}"; 
+        }
+    }
+}
---
+++ Back/src/proeventos.api/Models/evento.cs
@@ -0,0 +1,14 @@
+namespace proeventos.api.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QntPessoas { get; set; }
+        public string Lote { get; set; }
+        public string ImagemUrl { get; set; }
+        
+    }
+}
